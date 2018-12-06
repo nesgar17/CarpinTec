9d08a1ae74e91e143c3e6b117a0973e2d503863a
nesgar17 <39680968+nesgar17@users.noreply.github.com>
2018-12-06 02:30:54 -0600

    Creacion del Proyecto

---
+++ CarpinTec/CarpinTec/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarpinTec.MvcApplication" Language="C#" %>
---
+++ CarpinTec/CarpinTec/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarpinTec.Startup))]
+namespace CarpinTec
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
